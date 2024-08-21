dea7fa64a72fcee3b07dbdef56152cb7f2c99fa7
vishisht31 <vishishtmaroria31@gmail.com>
2024-08-21 18:39:16 +0530

    Favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import v from "../public/Images/V-logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <link rel="icon" href="../public/Images/V-logo.png"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
